1d5deea6b42106bf2be9cfa3e2302ad64e61312c
Michal Wronski <michal.robert.wronski@gmail.com>
2015-04-21 14:58:17 +0200

    [2015-B] Draft of pancake algorithm added.

---
+++ src/main/scala/com/mwronski/codejam2015/InfHousePancakes.scala
@@ -0,0 +1,65 @@
+package com.mwronski.codejam2015
+
+/**
+ * At the Infinite House of Pancakes, there are only finitely many pancakes,
+ * but there are infinitely many diners who would be willing to eat them!
+ * When the restaurant opens for breakfast, among the infinitely many diners, exactly D have non-empty plates;
+ * the ith of these has Pi pancakes on his or her plate. Everyone else has an empty plate.
+ *
+ * Normally, every minute, every diner with a non-empty plate will eat one pancake from his or her plate.
+ * However, some minutes may be special. In a special minute, the head server asks for the diners' attention,
+ * chooses a diner with a non-empty plate, and carefully lifts some number of pancakes off of that diner's plate
+ * and moves those pancakes onto one other diner's (empty or non-empty) plate. No diners eat during a special minute,
+ * because it would be rude.
+ *
+ * You are the head server on duty this morning, and it is your job to decide which minutes, if any,
+ * will be special, and which pancakes will move where. That is, every minute, you can decide to either
+ * do nothing and let the diners eat, or declare a special minute and interrupt the diners
+ * to make a single movement of one or more pancakes, as described above.
+ *
+ * Breakfast ends when there are no more pancakes left to eat.
+ * How quickly can you make that happen?
+ *
+ * @see https://code.google.com/codejam/contest/6224486/dashboard#s=p1
+ * @author Michal Wronski
+ */
+trait InfHousePancakes {
+
+  /**
+   * Minimum number of pancakes below which split is not viable
+   */
+  private final val minSplit = 4
+
+  /**
+   * Check how quickly given persons can eat their pancakes
+   * @param persons persons with pancakes on their plates
+   * @return non-negative number of turns
+   */
+  final def turns(persons: Array[Int]): Int = {
+    val pancakesCount = persons
+      .groupBy(pancakes => pancakes)
+      .mapValues(persons => persons.length)
+    val min = pancakesCount.min._1
+    if (min < minSplit) {
+      min
+    } else {
+      splitPancakes(pancakesCount)
+    }
+  }
+
+  /**
+   * Check how quickly pancakes can be eaten while split among persons
+   * @param pancakesCount number of pancakes (key) grouped by persons (value) who have to eat them
+   * @return non-negative number of turns
+   */
+  private def splitPancakes(pancakesCount: Map[Int, Int]): Int = {
+    val turns =
+      for (limit <- minSplit to pancakesCount.max._1)
+      yield pancakesCount
+        //check how quickly pancakes will be eaten using given limit
+        .map(pancakes => ((pancakes._1 - 1) / limit) * pancakes._2)
+        .sum
+    turns.min
+  }
+
+}
