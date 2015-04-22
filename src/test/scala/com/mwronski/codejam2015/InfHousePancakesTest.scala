529c590d98c3dce2067cd70ed59f6c6accdfb3f4
Michal Wronski <michal.robert.wronski@gmail.com>
2015-04-22 08:28:16 +0200

    [2015-B] File tasks attached to unit tests.

--- src/test/scala/com/mwronski/codejam2015/InfHousePancakesTest.scala
+++ src/test/scala/com/mwronski/codejam2015/InfHousePancakesTest.scala
@@ -2,6 +2,8 @@ package com.mwronski.codejam2015
 
 import org.scalatest.{Matchers, GivenWhenThen, FunSpec}
 
+import scala.io.Source
+
 /**
  * Unit tests related with pancakes problem
  *
@@ -11,24 +13,87 @@ import org.scalatest.{Matchers, GivenWhenThen, FunSpec}
 class InfHousePancakesTest extends FunSpec with GivenWhenThen with Matchers
 with InfHousePancakes {
 
-  describe("Sample Infinite House of Pancakes tasks") {
+  describe("2015-B: Infinite House of Pancakes") {
 
-    it("Should solve issue: 3") {
-      turns(Array(3)) should be(3)
+    describe("Sample tasks") {
+
+      it("Should solve issue: 3") {
+        turns(Array(3)) should be(3)
+      }
+
+      it("Should solve issue: 1 2 1 2") {
+        turns(Array(1, 2, 1, 2)) should be(2)
+      }
+
+      it("Should solve issue: 4") {
+        turns(Array(4)) should be(3)
+      }
+
+      it("Should solve issue: 4 8 7 8 3") {
+        turns(Array(4, 8, 7, 8, 3)) should be(7)
+      }
+
+      it("Should solve issue: 6 6") {
+        turns(Array(6, 6)) should be(5)
+      }
+
+      it("Should solve issue: 3 2 6") {
+        turns(Array(3, 2, 6)) should be(4)
+      }
+
     }
 
-    it("Should solve issue: 1 2 1 2") {
-      turns(Array(1, 2, 1, 2)) should be(2)
-    }
+    describe("File tasks") {
 
-    it("Should solve issue: 4") {
-      turns(Array(4)) should be(3)
-    }
+      it("Should solve small tasks") {
+        Given("small tasks")
+        val in = Source.fromFile("src/test/resources/2015/B-small-practice.in").getLines()
+        in.next() //skip number of test cases
+        And("solutions for small tasks ")
+        val out = Source.fromFile("src/test/resources/2015/B-small-practice.out").getLines()
 
-    it("Should solve issue: 4 8 7 8 3") {
-      turns(Array(4, 8, 7, 8, 3)) should be(7)
-    }
+        When("solving small tasks")
+        val solutions = scala.collection.mutable.ArrayBuffer[Int]()
+        while (in.hasNext) {
+          val numberOfPeople = in.next().toInt
+          val pancakes = in.next().split(" ").map(_.toInt)
+          solutions += turns(pancakes)
+        }
 
+        Then("all tasks are solved correctly")
+        var nr = 0
+        for (tc <- out) {
+          val expected = tc.split(":")(1).trim.toInt
+          solutions(nr) should be(expected)
+          nr += 1
+        }
+      }
+
+      it("Should solve large tasks") {
+        Given("large tasks")
+        val in = Source.fromFile("src/test/resources/2015/B-large-practice.in").getLines()
+        in.next() //skip number of test cases
+        And("solutions for large tasks ")
+        val out = Source.fromFile("src/test/resources/2015/B-large-practice.out").getLines()
+
+        When("solving large tasks")
+        val solutions = scala.collection.mutable.ArrayBuffer[Int]()
+        while (in.hasNext) {
+          val numberOfPeople = in.next().toInt
+          val pancakes = in.next().split(" ").map(_.toInt)
+          solutions += turns(pancakes)
+        }
+
+        Then("all tasks are solved correctly")
+        var nr = 0
+        for (tc <- out) {
+          val expected = tc.split(":")(1).trim.toInt
+          solutions(nr) should be(expected)
+          nr += 1
+        }
+      }
+
+    }
   }
 
 }
